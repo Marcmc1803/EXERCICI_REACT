01db906b1b167d895cc94ce6b565890fd338ec7f
manelcolominas <manel.colominas@estudiantat.upc.edu>
2026-03-17 19:17:51 +0100

    react repo 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import router from './router/router';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)

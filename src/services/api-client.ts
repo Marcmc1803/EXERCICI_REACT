347cbf83b494defbdea975853ab35087b5c924b2
jannogueira <jannogueiraacosta@gmail.com>
2026-03-18 22:24:05 +0100

    parche ip

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,7 +2,7 @@ import axios, { CanceledError } from 'axios';
 
 export default axios.create({
   //baseURL: 'https://jsonplaceholder.typicode.com',
-  baseURL: 'http://127.0.0.1:1337',
+  baseURL: 'http://localhost:1337',
 })
 
 export { CanceledError };
